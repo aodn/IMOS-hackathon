4139353aebaf764146d0665e14a941a34684575d
Jiayingabbyguo <abbyguo5678@foxmail.com>
2024-05-07 17:10:53 +1000

    Jiaying's script

---
+++ 2024/Projects/Fish-DAT/code/summary_metrics.R
@@ -0,0 +1,18 @@
+list.files()
+
+#shark
+# calculate these metrics:
+# distance traveled (cumulative; daily) #sf package "st_distance"
+#   Depth (min,max, mean) # as_
+#   Temperature (min,max, mean)
+# Distance to (shelf, shore)
+# Bathymetry (max depth at each position)
+
+
+
+# calculate these metrics:
+# distance traveled (cumulative; daily)
+#   Depth (min,max, mean)
+#   Temperature (min,max, mean)
+# Distance to (shelf, shore)
+# Bathymetry (max depth at each position)
