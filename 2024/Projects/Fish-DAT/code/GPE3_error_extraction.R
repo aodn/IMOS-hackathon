287e091cff4b1abf8df50c04c8cd21c8a41e3155
Vinay Udyawer <vinay.udyawer@gmail.com>
2024-05-09 07:58:50 +1000

    Update GPE3_error_extraction.R

--- 2024/Projects/Fish-DAT/code/GPE3_error_extraction.R
+++ 2024/Projects/Fish-DAT/code/GPE3_error_extraction.R
@@ -1,8 +1,18 @@
 ## Function to extract error polygons from the .kmz file
 
+# source("https://gist.githubusercontent.com/vinayudyawer/f84751026caa9cbc51e11e5b17a257af/raw/81e72c9b3eb16e8659fee55407d2f770cdb7dbd7/extract_polys.R")
+
+library(tidyverse)
+library(terra)
+library(tidync)
+library(sf)
+
 ## set how data are input into the function 
-## (it only requires the path to the folder where -Histo files are found)
+## (it only requires the path to the folder where -GPE3 files are found, and probability values)
 folder_path <- "~/Documents/GitHub/IMOS-hackathon/2024/Projects/Fish-DAT/data"
+
+## set probability contour 
+prob <- c(0.99, 0.95, 0.5)
 
 ## parse all the histo files in the folder
 csv_file <-
@@ -15,52 +25,59 @@ nc_files <-
   map(function(x){list.files(x, pattern = "*GPE3.nc", full.names = T)}) %>% 
   unlist()
 
-library(tidync)
+kmz_files <-
+  list.files(folder_path, pattern = '^[0-9]+$', full.names = T) %>% 
+  map(function(x){list.files(x, pattern = "*GPE3.kmz", full.names = T)}) %>% 
+  unlist()
 
-a <- 
-  tidync(nc_files[1]) %>% 
+
+## Grab ncdf error raster
+rast_timestamp <-
+  tidync(nc_files[1], what = "twelve_hour_timestamps") %>% 
   hyper_tibble() %>% 
+  mutate(twelve_hour_timestamps = as_datetime(twelve_hour_timestamps),
+         twelve_hour = as.character(twelve_hour))
+  
+error_rast <- 
+  tidync(nc_files[1], what = "twelve_hour_likelihoods") %>% 
+  hyper_tibble() %>% 
+  # left_join(rast_timestamp, by = "twelve_hour") %>% 
   dplyr::select(longitude, latitude, twelve_hour_likelihoods, twelve_hour) %>% 
-  pivot_wider(id_cols = c(1:2), names_from = "twelve_hour", values_from = "twelve_hour_likelihoods")
+  pivot_wider(id_cols = c(1:2), names_from = "twelve_hour", 
+              values_from = "twelve_hour_likelihoods") %>% 
+  rast(type = "xyz", crs = "epsg:4326") %>% 
+  disagg(fact = 10, method = "bilinear")
 
-error_rast <- rast(a, type = "xyz")
+scaled_error <- (error_rast - minmax(error_rast)[1,]) / (minmax(error_rast)[2,] - minmax(error_rast)[1,])
 
-plot(error_rast)
+fn <-
+  function(ras, levels){
+    vec <- as.contour(ras, levels = levels)
+    out <- st_as_sf(vec) %>% 
+      st_cast("POLYGON") %>% 
+      mutate(error_level = 1-level,
+             twelve_hour = names(ras))
+    out[-1]
+  }
 
-## input datasets
-dat <- 
-  read_csv("data/GPE3_output.csv", skip = 5) %>% 
-  mutate(Date = dmy_hms(Date)) %>% 
-  clean_names()
+error_contour <- 
+  scaled_error %>% 
+  as.list() %>% 
+  map(fn, levels = 1-prob) %>% 
+  bind_rows() %>% 
+  left_join(rast_timestamp, by = "twelve_hour") %>% 
+  dplyr::select(-twelve_hour) %>% 
+  rename(date_time = twelve_hour_timestamps)
 
-dat_sf <-
-  dat %>% 
-  st_as_sf(coords = c("most_likely_longitude", "most_likely_latitude"), crs = 4326, remove = F)
-
-## extact error polygons and calculate lat and lon error
-pols_05 <- extract_polys("data/GPE3_summary.kmz", prob_lim = 0.5) %>% st_make_valid() %>% rownames_to_column(var = "grouping")
-pols_95 <- extract_polys("data/GPE3_summary.kmz", prob_lim = 0.95) %>% st_make_valid() %>% rownames_to_column(var = "grouping")
-pols_99 <- extract_polys("data/GPE3_summary.kmz", prob_lim = 0.99) %>% st_make_valid() %>% rownames_to_column(var = "grouping")
-
-## combine into single polygons
-difference_list <- list()
-
-for (i in 1:nrow(pols_05)) {
-  diff1 <- pols_05[i,]
-  diff2 <- st_difference(pols_95[i, ], pols_05[i, ]) %>% select(-ends_with(".1"))
-  diff3 <- st_difference(pols_99[i, ], pols_95[i, ]) %>% select(-ends_with(".1"))
-  difference_list[[i]] <- rbind(diff1, diff2, diff3)
-}
-
-pols <- 
-  do.call("rbind", difference_list) %>% 
-  mutate(probs = as_factor(prob_lim*100),
-         date = datetime,
-         grouping = fct_inseq(grouping))
-
-
-
-
+ 
+mapview(error_contour, zcol = "error_level")@map %>%
+  addTimeslider(data = error_contour,
+                options = timesliderOptions(
+                  position = "topright",
+                  timeAttribute = "date_time",
+                  range = TRUE,
+                  alwaysShowDate = TRUE,
+                  sameDate = TRUE))
 
 
 
