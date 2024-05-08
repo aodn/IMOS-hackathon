fc6593d9640ce617d87b6ae50bbfa424dd6f05cf
dahliasan <laninaesloco@gmail.com>
2024-05-08 15:01:30 +1000

    refactor calculateDailyMetrics

--- 2024/Projects/Fish-DAT/code/calculateDailyMetrics.R
+++ 2024/Projects/Fish-DAT/code/calculateDailyMetrics.R
@@ -11,13 +11,18 @@ setwd("~/Documents/Projects/dev/IMOS-hackathon-1")
 # Get for one animal
 basedir <- "2024/Projects/Fish-DAT/data/47622"
 
-# Load the data
-filename <- dir(file.path(basedir), pattern = "daily-positions.csv", full.names = TRUE)
-d <- read_csv(filename); d; glimpse(d);
+
+
+
+
+calculate_daily_metrics <- function(basedir) {
+  # Load the data
+  filename <- dir(file.path(basedir), pattern = "daily-positions.csv", full.names = TRUE)
+  d <- read_csv(filename); d; glimpse(d)
 
 
 # Init output data
-out <- NULL 
+out <- d
 
 # Load hi-res series data
 
@@ -35,39 +40,63 @@ if (file.exists(file.path(basedir, "Series.csv"))) {
       MaxDepthFromSeries = max(Depth, na.rm = TRUE),
       MeanDepthFromSeries = mean(Depth, na.rm = TRUE)
     ) %>% 
-    select(Ptt, Day, MinTempFromSeries, MeanTempFromSeries, MaxTempFromSeries, MinDepthFromSeries, MaxDepthFromSeries, MeanDepthFromSeries); env_dat; glimpse(env_dat)
+    select(Ptt, Day, MinTempFromSeries, MeanTempFromSeries, MaxTempFromSeries, MinDepthFromSeries, MaxDepthFromSeries, MeanDepthFromSeries)
   
-  out <- left_join(d, series_dat, by = c("Date" = "Day", "Ptt" = "Ptt"))
+  # env_dat; glimpse(env_dat)
   
-} 
+  out <- left_join(out, series_dat, by = c("Date" = "Day", "Ptt" = "Ptt"))
+  
+}  else {
+  # message
+  message("No Series.csv found in folder")
+}
   
 
 # IF animal doesn't have Series.csv use the summary data
 # use DailyData.csv
-filename <- dir(file.path(basedir), pattern = "DailyData.csv", full.names = TRUE)
-daily_dat <- read_csv(filename) %>% 
-  mutate(Day = mdy(Date)) %>% 
-  select(Ptt, Day, MinTemp, MaxTemp, MinDepth, MaxDepth); daily_dat; glimpse(daily_dat)
+
+if(
+  file.exists(file.path(basedir, "DailyData.csv"))
+) {
+  filename <- dir(file.path(basedir), pattern = "DailyData.csv", full.names = TRUE)
+  daily_dat <- read_csv(filename) %>% 
+    mutate(Day = mdy(Date)) %>% 
+    select(Ptt, Day, MinTemp, MaxTemp, MinDepth, MaxDepth); 
+  
+  # daily_dat
+  # glimpse(daily_dat)
+  
+  out <- left_join(out, daily_dat, by = c("Date" = "Day", "Ptt" = "Ptt"))
+} else {
+  # message
+  message("No DailyData.csv found in folder")
+}
 
 
-# use PDTs.csv
-filename <- dir(file.path(basedir), pattern = "PDTs.csv", full.names = TRUE)
-pdt_dat <- read_csv(filename) %>% 
-  # split Date by " " and create string of second element + first element
-  mutate(Date = str_split(Date, pattern = " ")) %>% 
-  mutate(Date = map_chr(Date, ~paste(.x[2], .x[1], sep = " "))) %>% 
-  mutate(Date = dmy_hms(Date)) %>% 
-  mutate(Day = as.Date(Date)) %>% 
-  ungroup() %>% 
-  mutate(MeanTempPDT = rowMeans(select(., contains("Temp")), na.rm = TRUE)) %>% 
-  mutate(MeanDepthPDT = rowMeans(select(., contains("Depth")), na.rm = TRUE)) %>% 
-  select(Ptt, Day, MeanTempPDT, MeanDepthPDT); pdt_dat; 
+if (
+  file.exists(file.path(basedir, "PDTs.csv"))
+) {
+  # use PDTs.csv
+  filename <- dir(file.path(basedir), pattern = "PDTs.csv", full.names = TRUE)
+  pdt_dat <- read_csv(filename) %>% 
+    # split Date by " " and create string of second element + first element
+    mutate(Date = str_split(Date, pattern = " ")) %>% 
+    mutate(Date = map_chr(Date, ~paste(.x[2], .x[1], sep = " "))) %>% 
+    mutate(Date = dmy_hms(Date)) %>% 
+    mutate(Day = as.Date(Date)) %>% 
+    mutate(MeanTempPDT = rowMeans(select(., contains("Temp")), na.rm = TRUE)) %>% 
+    mutate(MeanDepthPDT = rowMeans(select(., contains("Depth")), na.rm = TRUE)) %>% 
+    select(Ptt, Day, MeanTempPDT, MeanDepthPDT); pdt_dat; 
+  
+  # pdt_dat
+  # glimpse(pdt_dat)
+  
+  out <- left_join(out, pdt_dat, by = c("Date" = "Day", "Ptt" = "Ptt"))
+} else {
+  # message
+  message("No PDTs.csv found in folder")
+}
 
-glimpse(pdt_dat)
 
-
-
-# Append daily_dat to out
-out <- out %>% 
-  left_join(., daily_dat, by = c("Date" = "Day", "Ptt" = "Ptt")) %>% 
-  left_join(., pdt_dat, by = c("Date" = "Day", "Ptt" = "Ptt"))
+return(out)
+}
