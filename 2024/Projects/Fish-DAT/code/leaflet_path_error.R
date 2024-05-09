4067b10b57b3b9e674933da1fef55a47c4211512
Vinay Udyawer <vinay.udyawer@gmail.com>
2024-05-09 13:45:24 +1000

    Update leaflet_path_error.R

--- 2024/Projects/Fish-DAT/code/leaflet_path_error.R
+++ 2024/Projects/Fish-DAT/code/leaflet_path_error.R
@@ -5,26 +5,22 @@ source("https://raw.githubusercontent.com/aodn/IMOS-hackathon/fishdat/2024/Proje
 
 library(tidyverse)
 library(sf)
+library(lubridate)
+library(mapview)
 library(leaflet)
 library(leaflet.extras2)
 library(terra)
 library(tidync)
 
 
-# Set up a function all we provide is the location of the folder and metadata and it plots the movement with the errors
-
-## data input required
-# folder_name: location of folder with all the MiniPAT files and GPE3 outputs
-# metadata: a metadata data.frame that 
-# prop : a vector of probabilities for the error 
-
 folder_path <- "~/Documents/GitHub/IMOS-hackathon/2024/Projects/Fish-DAT/data"
 metadata <- list.files(folder_path, pattern = "*Metadata*.csv") %>% read_csv()
-prob <- c(0.99, 0.95, 0.5)
+prob <- c(0.99)#, 0.95, 0.5)
+colpal <- list.files(folder_path, pattern = "*colour_palette*.csv") %>% read_csv()
 i = 1
 
-leaflet_path_error <-
-  function(folder_name, metadata, prop, .verbose = F){
+# plot_path <-
+#   function(folder_name, metadata, prob = 0.99, colpal){
     
     ## parse all the histo files in the folder
     csv_file <-
@@ -42,6 +38,7 @@ leaflet_path_error <-
     #   map(function(x){list.files(x, pattern = "*GPE3.kmz", full.names = T)}) %>% 
     #   unlist()
     
+    id <- basename(dirname(csv_file))
     
     if(.verbose){
       message("Grabbing GPE3 output to map...") 
@@ -50,32 +47,42 @@ leaflet_path_error <-
     ## Lets convert the GPE3 csv_file data into a spatial object with positions per month
     pos_dat <-
       read_csv(csv_file, skip = 5) %>% 
+      mutate(date_time = dmy_hms(Date),
+             date = lubridate::date(date_time),
+             month = lubridate::month(date, label = T, abbr = T)) %>% 
       transmute(Ptt = factor(Ptt), 
                 lon = `Most Likely Longitude`,
                 lat = `Most Likely Latitude`,
-                col = viridisLite::cividis(n_distinct(Ptt))[Ptt])
+                date,
+                month) %>% 
+      left_join(colpal, by = "month")
       
-    # pos_sf <-
-    #   pos_dat %>% 
-    #   group_by(Ptt) %>% 
-    #   st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = F)
+    pos_sf <-
+      pos_dat %>%
+      group_by(Ptt) %>%
+      st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = F) %>% 
+      st_shift_longitude()
     
-    # pos_path <-
-    #   pos_sf %>% 
-    #   summarise(do_union = F) %>% 
-    #   st_cast("LINESTRING")
+    pos_path <-
+      pos_sf %>%
+      summarise(do_union = F) %>%
+      st_cast("LINESTRING")
     
     ## Extract error polygons
-    pols <- gpe3_error_polys(folder_path, prob, verbose = .verbose)
+    pols <- 
+      gpe3_error_polys(folder_path = folder_path, 
+                       prob = prob, 
+                       verbose = .verbose) %>% 
+      
     
     
     ## Leaflet plot
     mytext = paste(
-      "Date: ", track$`Date`, "<br/>",
-      "Ptt: ", track$Ptt, "<br/>",
-      "Days at liberty: ", track$`day.at.liberty`, "<br/>",
-      "Longitude: ", track$Longitude, "<br/>",
-      "Latitude: ", track$Latitude, sep="") %>%
+      "Date: ", pos_dat$date, "<br/>",
+      "Ptt: ", pos_dat$Ptt, "<br/>",
+      # "Days at liberty: ", pos_dat$`day.at.liberty`, "<br/>",
+      "Longitude: ", pos_dat$lon, "<br/>",
+      "Latitude: ", pos_dat$lat, sep="") %>%
       lapply(htmltools::HTML)
     
     # myleafletplot <-
@@ -84,29 +91,29 @@ leaflet_path_error <-
       addProviderTiles(providers$OpenStreetMap, group="Map") %>%   # Street Map view
       addProviderTiles(providers$Esri.WorldImagery, group="Satellite") %>%   # typical Google Earth satellite view
       # Add location data:
-      addPolylines(lng = track_sf$Longitude, lat = track_sf$Latitude, 
+      addPolylines(lng = pos_sf$lon, lat = pos_sf$lat, group = pos_sf$Ptt,
                    color = "white", weight = 1.5,
                    labelOptions = labelOptions(noHide = TRUE)) %>%
       # add the tag detection data
-      addCircleMarkers(lng = track_sf$Longitude, lat = track_sf$Latitude, 
-                       weight = 2, radius = 4, color = track_sf$colour,
+      addCircleMarkers(lng = pos_sf$lon, lat = pos_sf$lat, group = pos_sf$Ptt,
+                       weight = 2, radius = 4, color = pos_sf$colour,
                        stroke = FALSE, fillOpacity = 1, 
-                       group = track_sf$month,
+                       # group = pos_sf$month,
                        label=mytext) %>%  # don’t forget to assign a group to the markers
       # Layers control
       addLayersControl(
         overlayGroups = c("Satellite"),  # specify the desired basemap options for the output map
-        baseGroups = as.character(unique(track_sf$Ptt)),  # add the data groups to overlay on the map
+        baseGroups = as.character(unique(pos_dat$Ptt)),  # add the data groups to overlay on the map
         options = layersControlOptions(collapsed = FALSE, position = "topright")) %>%
       # Add legend
       addLegend(position = "topleft",
-                colors = myColors, labels = names(myColors),
+                colors = colpal$colour, labels = colpal$month,
                 title = "Month:",
                 opacity = 1
       ) %>% 
       leaflet::hideGroup("Satellite")
     
-  }
+  # }
 
 
 
