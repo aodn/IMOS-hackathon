6b8b4cea6e92e3ce5a612123a66d158b8f822487
David Green <davo.b.green@gmail.com>
2024-05-09 11:41:28 +1000

    functioning script

--- 2024/Projects/Fish-DAT/code/HistoPlot.R
+++ 2024/Projects/Fish-DAT/code/HistoPlot.R
@@ -1,100 +1,102 @@
-
+# 
 library(tidyverse)
 library(lubridate)
 library(patchwork)
-
-histoviz <- function(folder_path){
-  
-  ## parse all the histo files in the folder
-  file_names <-
-    list.files(folder_path, pattern = '^[0-9]+$', full.names = T) %>% 
-    map(function(x){list.files(x, pattern = "*Histos.csv", full.names = T)}) %>% 
-    unlist()
-  
-  ### Lets work with the first histo file.. this can be looped across all the files in histos
-  fn <- function(file_names){
-    
-    ## Read in the file
-    histos <- read_csv(file_names[1], show_col_types = F)
-    
-    ## Define the Time at Depth (tad) and Time at Temperature (tat) bins
-    tad_bins <-
-      histos %>% 
-      filter(HistType %in% "TADLIMITS") %>% 
-      dplyr::select(contains("Bin")) %>% 
-      pivot_longer(-1, names_to = "Bins", values_to = "bin") %>% 
-      dplyr::select(-NumBins) %>% 
-      filter(!is.na(bin)) %>% 
-      mutate(variable = "depth")
-    
-    tat_bins <-
-      histos %>% 
-      filter(HistType %in% "TATLIMITS") %>% 
-      dplyr::select(contains("Bin")) %>% 
-      pivot_longer(-1, names_to = "Bins", values_to = "bin") %>% 
-      dplyr::select(-NumBins) %>% 
-      filter(!is.na(bin)) %>% 
-      mutate(variable = "temp")
-    
-    
-    ## subset and configure depth and temp data into a data.frame
-    depth_dat <-
-      histos %>% 
-      filter(HistType %in% c("TAD")) %>% 
-      mutate(date = lubridate::dmy_hms(paste(str_split(Date, pattern = " ", simplify = T)[,2],
-                                             str_split(Date, pattern = " ", simplify = T)[,1])),
-             id = as.character(Ptt)) %>% 
-      dplyr::select(id, date, contains("Bin"), -NumBins) %>%
-      pivot_longer(-c(date, id), names_to = "Bins", values_to = "prop") %>% 
-      left_join(tad_bins, by = "Bins") %>% 
-      dplyr::select(-Bins)
-    
-    
-    temp_dat <-
-      histos %>% 
-      filter(HistType %in% c("TAT")) %>% 
-      mutate(date = lubridate::dmy_hms(paste(str_split(Date, pattern = " ", simplify = T)[,2],
-                                             str_split(Date, pattern = " ", simplify = T)[,1])),
-             id = as.character(Ptt)) %>% 
-      dplyr::select(id, date, contains("Bin"), -NumBins) %>%
-      pivot_longer(-c(date, id), names_to = "Bins", values_to = "prop") %>% 
-      left_join(tat_bins, by = "Bins") %>% 
-      dplyr::select(-Bins)
-    
-    combdat <- bind_rows(depth_dat, temp_dat)
-    
-    return(combdat)
-  }
-  
-  outdat <- 
-    map(.x = file_names, .f = fn, .progress = TRUE) %>% 
-    list_rbind() %>% 
-    mutate(bin = as.factor(bin)) %>% 
-    filter(!is.na(variable))
-  
-  return(outdat)
-}
-
-folder_path <- "2024/Projects/Fish-DAT/data/"
-
-plot_dat <- histoviz(folder_path)
+library(fs)
+# 
+# histoviz <- function(folder_path){
+#   
+#   ## parse all the histo files in the folder
+#   file_names <-
+#     list.files(folder_path, pattern = '^[0-9]+$', full.names = T) %>% 
+#     map(function(x){list.files(x, pattern = "*Histos.csv", full.names = T)}) %>% 
+#     unlist()
+#   
+#   ### Lets work with the first histo file.. this can be looped across all the files in histos
+#   fn <- function(file_names){
+#     
+#     ## Read in the file
+#     histos <- read_csv(file_names[1], show_col_types = F)
+#     
+#     ## Define the Time at Depth (tad) and Time at Temperature (tat) bins
+#     tad_bins <-
+#       histos %>% 
+#       filter(HistType %in% "TADLIMITS") %>% 
+#       dplyr::select(contains("Bin")) %>% 
+#       pivot_longer(-1, names_to = "Bins", values_to = "bin") %>% 
+#       dplyr::select(-NumBins) %>% 
+#       filter(!is.na(bin)) %>% 
+#       mutate(variable = "depth")
+#     
+#     tat_bins <-
+#       histos %>% 
+#       filter(HistType %in% "TATLIMITS") %>% 
+#       dplyr::select(contains("Bin")) %>% 
+#       pivot_longer(-1, names_to = "Bins", values_to = "bin") %>% 
+#       dplyr::select(-NumBins) %>% 
+#       filter(!is.na(bin)) %>% 
+#       mutate(variable = "temp")
+#     
+#     
+#     ## subset and configure depth and temp data into a data.frame
+#     depth_dat <-
+#       histos %>% 
+#       filter(HistType %in% c("TAD")) %>% 
+#       mutate(date = lubridate::dmy_hms(paste(str_split(Date, pattern = " ", simplify = T)[,2],
+#                                              str_split(Date, pattern = " ", simplify = T)[,1])),
+#              id = as.character(Ptt)) %>% 
+#       dplyr::select(id, date, contains("Bin"), -NumBins) %>%
+#       pivot_longer(-c(date, id), names_to = "Bins", values_to = "prop") %>% 
+#       left_join(tad_bins, by = "Bins") %>% 
+#       dplyr::select(-Bins)
+#     
+#     
+#     temp_dat <-
+#       histos %>% 
+#       filter(HistType %in% c("TAT")) %>% 
+#       mutate(date = lubridate::dmy_hms(paste(str_split(Date, pattern = " ", simplify = T)[,2],
+#                                              str_split(Date, pattern = " ", simplify = T)[,1])),
+#              id = as.character(Ptt)) %>% 
+#       dplyr::select(id, date, contains("Bin"), -NumBins) %>%
+#       pivot_longer(-c(date, id), names_to = "Bins", values_to = "prop") %>% 
+#       left_join(tat_bins, by = "Bins") %>% 
+#       dplyr::select(-Bins)
+#     
+#     combdat <- bind_rows(depth_dat, temp_dat)
+#     
+#     return(combdat)
+#   }
+#   
+#   outdat <- 
+#     map(.x = file_names, .f = fn, .progress = TRUE) %>% 
+#     list_rbind() %>% 
+#     mutate(bin = as.factor(bin)) %>% 
+#     filter(!is.na(variable))
+#   
+#   return(outdat)
+# }
+# 
 
 
-head(plot_dat)
-
-
-histoplot <- function(tag_ids = c("47618", "47622", "227150", "227151"),
+histoplot <- function(tag_ids = c("47618"),
                       folder_path = "2024/Projects/Fish-DAT/data/",
                       TAD = TRUE,
                       TAT = TRUE,
-                      species = TRUE,
-                      OO_Shelf = TRUE){
-  
+                      species = TRUE){
+  suppressMessages({
   plot_dat <- histoviz(folder_path) %>% 
-    filter(id %in% tag_ids) #%>% 
-    # mutate(bin = as.numeric(as.character(bin)))
+    filter(id %in% tag_ids) 
   
-  plot_dat$species = "Cool species"
+  # Get meta data
+  # plot_dat$species = "Cool species"
+  spec_dat <- fs::dir_ls(folder_path, glob = "*metadata*.csv", ignore.case = TRUE)
+  spec_dat <- read_csv(spec_dat) %>% 
+    rename_with(.cols = everything(), .fn = tolower) %>% 
+    mutate(id = as.character(ptt_id)) %>% 
+    select(-ptt_id)
+  
+  plot_dat <- plot_dat %>% 
+    left_join(spec_dat)
   
   if(species){  
     spec_dat <- plot_dat %>% 
@@ -115,8 +117,9 @@ histoplot <- function(tag_ids = c("47618", "47622", "227150", "227151"),
     ungroup()
     
   
-  # Simple bar plots of TAT
-  if(TAT){  
+  # Time at Temperature plots
+  
+  if(TAT){  # do we want it plotted?
     g_TAT <- ggplot(plot_dat %>% filter(variable == "temp"),
                mapping = aes(y = bin,
                              x = prop_mean,
@@ -132,30 +135,46 @@ histoplot <- function(tag_ids = c("47618", "47622", "227150", "227151"),
                         colour = "grey50",
                         width = 0.3) +
         
-          scale_fill_viridis_d(name = "Tag ID") +
+          scale_fill_viridis_d(name = "Tag ID", position = "top") +
           scale_colour_viridis_d(name = "Tag ID") +
-          coord_cartesian(expand = TRUE,
-                          # ylim = range(plot_dat %>% 
-                          #                filter(variable == "temp") %>% 
-                          #                pull(bin)
-                          #              )
+          coord_cartesian(expand = TRUE
                           ) +
-          
-          # geom_density(alpha = 0.2) +
           ylab(expr(paste("Temp ("^"o","C)"))) +
           xlab("Proportion of time (%)") +
-          theme_bw() 
-    g_TAT
+      
+          theme_bw() +
+      theme(legend.position = "top")
+      
+      
+    
+    if(species){ # adding species level plot if wanted
+      sp_TAT <- ggplot(spec_dat %>% filter(variable == "temp"),
+                       mapping = aes(y = bin,
+                                     weight = prop_mean,
+                                     fill = species,
+                                     group = species)) +
+        geom_density(alpha = 0.5) +
+        theme_bw() +
+        scale_fill_viridis_d() +
+        coord_cartesian(expand = TRUE) +
+        ylab("") +
+        xlab("Proportion of time (%)") +
+        theme(legend.position = "top")
+        
+        g_TAT <- g_TAT + sp_TAT
+    
+      }
+    
   }
-  if(TAD){
+  
+  # Time at depth plots
+  if(TAD){ # do we want it plotted?
       # Simple bar plots of TAD
      g_TAD <- ggplot(plot_dat %>% filter(variable == "depth"),
                mapping = aes(y = bin,
                              x = prop_mean,
                              group = id,
-                             # colour = id,
-                             fill = id,
-                             # weight = prop_mean
+                             fill = id
                )) +
           geom_bar(stat = "identity", alpha = 0.5) +
           geom_errorbar(aes(xmin = prop_mean - prop_sd,
@@ -165,23 +184,43 @@ histoplot <- function(tag_ids = c("47618", "47622", "227150", "227151"),
           
           scale_fill_viridis_d(name = "Tag ID") +
           scale_colour_viridis_d(name = "Tag ID") +
-          coord_cartesian(expand = FALSE) +
-          # coord_cartesian(expand = FALSE) +
-          # geom_density(alpha = 0.2) +
-          # scale_y_reverse() +
+          coord_cartesian(expand = TRUE) +
+          scale_y_discrete(limits=rev) +
           ylab("Depth (m)") +
           xlab("Proportion of time (%)") +
-          theme_bw() 
-     # if(species){
-     #   g_TAD + geom_path(data = spec_dat %>% filter(variable == "depth"),
-     #                     mapping = aes(y = bin,
-     #                                   # group = id,
-     #                                   # colour = id,
-     #                                   # fill = id,
-     #                                   x = prop_mean)
-     #   )
-     # }
+          theme_bw()  +
+          theme(legend.position = "top")
+     
+     if(species){ # adding species level plot if wanted
+       sp_TAD <- ggplot(spec_dat %>% filter(variable == "depth"),
+                        mapping = aes(y = bin,
+                                      weight = prop_mean,
+                                      fill = species,
+                                      group = species)) +
+         geom_density(alpha = 0.5) +
+         theme_bw() +
+         scale_fill_viridis_d() +
+         coord_cartesian(expand = TRUE) +
+         scale_y_discrete(limits=rev) +
+         # ylab(expr(paste("Temp ("^"o","C)"))) +
+         ylab("") +
+         xlab("Proportion of time (%)") +
+         theme(legend.position = "top")
+       
+       g_TAD <- g_TAD + sp_TAD
+       
+     }
   }
   
-  
+  # Deciding which plots to return
+  if(TAD & TAT){
+    return(g_TAD/g_TAT)
+  }
+  if(TAD & !TAT){
+    return(g_TAD)
+  }
+  if(!TAD & TAT){
+    return(g_TAT)
+  }
+  })
 }
