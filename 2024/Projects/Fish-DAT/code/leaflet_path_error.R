4a755ba55fadf20752b521d4decf494757e3f6d0
Vinay Udyawer <vinay.udyawer@gmail.com>
2024-05-09 14:24:14 +1000

    Update leaflet_path_error.R

--- 2024/Projects/Fish-DAT/code/leaflet_path_error.R
+++ 2024/Projects/Fish-DAT/code/leaflet_path_error.R
@@ -5,6 +5,7 @@ source("https://raw.githubusercontent.com/aodn/IMOS-hackathon/fishdat/2024/Proje
 
 library(tidyverse)
 library(sf)
+library(terra)
 library(lubridate)
 library(mapview)
 library(leaflet)
@@ -14,15 +15,18 @@ library(tidync)
 
 
 folder_path <- "~/Documents/GitHub/IMOS-hackathon/2024/Projects/Fish-DAT/data"
-metadata <- list.files(folder_path, pattern = "*Metadata*.csv") %>% read_csv()
+metadata <- list.files(folder_path, pattern = "*Metadata*.csv", full.names = T) %>% read_csv()
 prob <- c(0.99)#, 0.95, 0.5)
-colpal <- list.files(folder_path, pattern = "*colour_palette*.csv") %>% read_csv()
-i = 1
+colpal <- list.files(folder_path, pattern = "*colour_palette*.csv", full.names = T) %>% read_csv()
+
+## Fishery data
+fishing <- list.files(folder_path, pattern = "*GFW*.csv", full.names = T) %>% read_csv()
+  
 
 # plot_path <-
 #   function(folder_name, metadata, prob = 0.99, colpal){
     
-    ## parse all the histo files in the folder
+    ## parse all the csv and ncdf files in the folder
     csv_file <-
       list.files(folder_path, pattern = '^[0-9]+$', full.names = T) %>% 
       map(function(x){list.files(x, pattern = "*GPE3.csv", full.names = T)}) %>% 
@@ -33,16 +37,11 @@ i = 1
       map(function(x){list.files(x, pattern = "*GPE3.nc", full.names = T)}) %>% 
       unlist()
     
-    # kmz_files <-
-    #   list.files(folder_path, pattern = '^[0-9]+$', full.names = T) %>% 
-    #   map(function(x){list.files(x, pattern = "*GPE3.kmz", full.names = T)}) %>% 
-    #   unlist()
-    
     id <- basename(dirname(csv_file))
     
-    if(.verbose){
-      message("Grabbing GPE3 output to map...") 
-    }
+    # if(.verbose){
+    #   message("Grabbing GPE3 output to map...") 
+    # }
     
     ## Lets convert the GPE3 csv_file data into a spatial object with positions per month
     pos_dat <-
@@ -71,10 +70,14 @@ i = 1
     ## Extract error polygons
     pols <- 
       gpe3_error_polys(folder_path = folder_path, 
-                       prob = prob, 
-                       verbose = .verbose) %>% 
+                       prob = 0.99, 
+                       verbose = F) %>% 
+      mutate(Ptt = factor(Ptt))
       
-    
+    pol_sum <-
+      pols %>% 
+      st_shift_longitude()
+      
     
     ## Leaflet plot
     mytext = paste(
@@ -85,25 +88,73 @@ i = 1
       "Latitude: ", pos_dat$lat, sep="") %>%
       lapply(htmltools::HTML)
     
+    # m1 <- 
+      mapview(pos_sf, zcol = "month", burst = T, alpha = 0, alpha.regions = 1,
+              cex = 2, legend = F, homebutton = F) +
+      mapview(pos_path, burst = T, cex = 2, legend = F, homebutton = F)
+        
+    
+    m1@map %>% 
+      addTimeslider(data = pol_sum %>% filter(error_level %in% "0.99"),
+                    stroke = FALSE,
+                    options = timesliderOptions(
+                      position = "bottomright",
+                      timeAttribute = "date_time",
+                      range = FALSE,
+                      alwaysShowDate = TRUE,
+                      sameDate = TRUE,
+                      follow = TRUE))
+    
     # myleafletplot <-
     leaflet() %>%
       # Base groups (you can add multiple basemaps):
       addProviderTiles(providers$OpenStreetMap, group="Map") %>%   # Street Map view
       addProviderTiles(providers$Esri.WorldImagery, group="Satellite") %>%   # typical Google Earth satellite view
       # Add location data:
-      addPolylines(lng = pos_sf$lon, lat = pos_sf$lat, group = pos_sf$Ptt,
-                   color = "white", weight = 1.5,
-                   labelOptions = labelOptions(noHide = TRUE)) %>%
-      # add the tag detection data
-      addCircleMarkers(lng = pos_sf$lon, lat = pos_sf$lat, group = pos_sf$Ptt,
-                       weight = 2, radius = 4, color = pos_sf$colour,
-                       stroke = FALSE, fillOpacity = 1, 
-                       # group = pos_sf$month,
-                       label=mytext) %>%  # don’t forget to assign a group to the markers
+      # addPolylines(lng = pos_sf$lon, lat = pos_sf$lat, 
+      #              group = pos_sf$Ptt,
+      #              weight = 1.5,
+      #              labelOptions = labelOptions(noHide = TRUE)) %>%
+      # # add the tag detection data
+      # addCircleMarkers(lng = pos_sf$lon, lat = pos_sf$lat, group = pos_sf$Ptt,
+      #                  weight = 2, radius = 4, color = pos_sf$colour,
+      #                  stroke = FALSE, fillOpacity = 1, 
+      #                  # group = pos_sf$month,
+      #                  label=mytext) %>%  # don’t forget to assign a group to the markers
+      addTimeslider(data = pol_sum %>% filter(error_level %in% "0.99"),
+                    stroke = FALSE,
+                    options = timesliderOptions(
+                      position = "bottomright",
+                      timeAttribute = "date_time",
+                      range = FALSE,
+                      alwaysShowDate = TRUE,
+                      sameDate = TRUE,
+                      follow = TRUE)) %>%
+      addTimeslider(data = pos_sf,
+                    radius = 5,
+                    opacity = 0.9,
+                    color = pos_sf$colour,
+                    stroke = FALSE,
+                    options = timesliderOptions(
+                      position = "bottomright",
+                      timeAttribute = "date",
+                      range = FALSE,
+                      alwaysShowDate = TRUE,
+                      sameDate = TRUE,
+                      follow = TRUE)) %>%
+      # addTimeslider(data = pos_path,
+      #               stroke = FALSE,
+      #               options = timesliderOptions(
+      #                 position = "bottomright",
+      #                 timeAttribute = "date_time",
+      #                 range = FALSE,
+      #                 alwaysShowDate = TRUE,
+      #                 sameDate = TRUE,
+      #                 follow = TRUE)) %>% 
       # Layers control
       addLayersControl(
         overlayGroups = c("Satellite"),  # specify the desired basemap options for the output map
-        baseGroups = as.character(unique(pos_dat$Ptt)),  # add the data groups to overlay on the map
+        baseGroups = as.character(unique(pos_sf$Ptt)),  # add the data groups to overlay on the map
         options = layersControlOptions(collapsed = FALSE, position = "topright")) %>%
       # Add legend
       addLegend(position = "topleft",
