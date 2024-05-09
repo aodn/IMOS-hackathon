3f5e951469363656c1b9fb2c069e16f2a4d3db19
fjaine <fabricejaine@gmail.com>
2024-05-09 12:36:01 +1000

    Summary metrics & radar plot script

--- 2024/Projects/Fish-DAT/code/RadarViz.R
+++ 2024/Projects/Fish-DAT/code/RadarViz.R
@@ -7,108 +7,116 @@ rm(list=ls()) # to clear workspace
 # Set working directory
 setwd("~/Documents/GitHub/IMOS-hackathon/2024/Projects/Fish-DAT/") # set manually to your own working directory
 
-# Load the data
-library(data.table)
-dat<-read.csv("outputs/47618_summaries.csv",header=TRUE); head(dat)
-ptt_id = unique(dat$Ptt)
-
+# Load the metadata
 library(data.table)
 met<-read.csv("data/HackathonMetadata.csv",header=TRUE); head(met)
 
 #--------------------------------
-# Compute overall min/mean/max summary metrics for each fish
-library(dplyr)
 
-daily_measurements <- dat %>%
-  group_by(Date) %>%
-  summarise(days_at_liberty = max(day.at.liberty),
-            mean_daily_distance = mean(DistTravelled),
-            distance_total = max(DistfromStart),
-            mean_dist_to_shelf = mean(DistToShelf),
-            mean_distance_to_shore = mean(DistToShore),
-            min_temp = min(MinTempFromSeries), mean_temp = mean(MeanTempFromSeries), max_temp = max(MaxTempFromSeries),
-            min_depth = min(MinDepthFromSeries), mean_depth = mean(MeanDepthFromSeries), max_depth = max(MaxDepthFromSeries),
-            max_bathy = max(bathy_depth)
-            )
-daily_measurements
+# Loading all datasets at once
 
-library(dplyr)
-daily_measurements <- daily_measurements %>%
-  mutate(mean_daily_distance = if_else(is.na(mean_daily_distance), 0, mean_daily_distance)) %>%
-  filter(!is.na(mean_temp)) %>% mutate(mean_temp = mean(mean_temp)) %>%
-  filter(!is.na(mean_depth)) %>% mutate(mean_depth = mean(mean_depth))
+all_files <- list.files("~/Documents/GitHub/IMOS-hackathon/2024/Projects/Fish-DAT/data/outputs/",
+                             pattern = "_summaries", recursive = T,
+                             full.names = T)
 
+# Looping through each tag and computing and saving summary statistics
 
-summary_metrics <- daily_measurements %>%
-  summarise(days_at_liberty = max(days_at_liberty),
-            mean_daily_distance = mean(mean_daily_distance),
-            distance_total = max(distance_total),
-            mean_dist_to_shelf = mean(mean_dist_to_shelf),
-            mean_distance_to_shore = mean(mean_distance_to_shore),
-            min_temp = min(min_temp), mean_temp = mean(mean_temp), max_temp = max(max_temp),
-            min_depth = min(min_depth), mean_depth = mean(mean_depth), max_depth = max(max_depth),
-            max_bathy = -max(max_bathy)
+for(i in 1:length(all_files)){
+  # read each file
+  dat <- read.csv(all_files[i], header=T); head(dat)
+  ptt_id = unique(dat$Ptt); print(ptt_id)
+  # compute daily min/mean/max measurements
+  library(dplyr)
+  daily_measurements <- dat %>%
+    mutate(mean_daily_distance = if_else(is.na(DistTravelled), 0, DistTravelled)) %>%
+              filter(!is.na(MinTemp)) %>% mutate(min_temp = min(MinTemp)) %>%
+              filter(!is.na(MeanTempPDT)) %>% mutate(mean_temp = mean(MeanTempPDT)) %>%
+              filter(!is.na(MeanDepthPDT)) %>% mutate(mean_depth = mean(MeanDepthPDT))
+  daily_measurements
+  # compute overall summary metrics
+  summary_metrics <- daily_measurements %>%
+    reframe(Ptt = unique(ptt_id),
+            DaysAtLiberty = unique(max(day.at.liberty)),
+            mean_DailyDist = unique(mean(mean_daily_distance)),
+            total_Dist = unique(max(DistfromStart)),
+            mean_DistToShelf = unique(mean(DistToShelf)),
+            mean_DistToShore = unique(mean(DistToShore)),
+            min_Temp = unique(min_temp), mean_Temp = unique(mean_temp), max_Temp = unique(max(MaxTemp)),
+            min_Depth = unique(min(MinDepth)), mean_Depth = unique(mean_depth), max_Depth = unique(max(MaxDepth)),
+            max_Bathy = unique(max(bathy_depth))
   )
-summary_metrics
+  # save output in temp df
+  if(i %in% 1){
+    out_dat <- summary_metrics
+  } else {
+    out_dat <-
+      out_dat %>% 
+      bind_rows(summary_metrics)
+  }
+}
+#out_dat
+met <- left_join(met, out_dat, by=c("Ptt")); print(met)
+#write.csv(met, "data/HackathonMetadata.csv", colnames=F)
 
-#Convert the data to dataframe
-summary_metrics <- as.data.frame(summary_metrics); head(summary_metrics)
+# Setting max scales based on all available values
 
-drop.cols <- c('distance_total', 'max_bathy')
-library(dplyr)
-summary_metrics <- summary_metrics %>% select(-drop.cols)
-
-
-#--------------------------------
-# Library
 library(fmsb)
-
 # To use the fmsb package with several individuals, I have to add 2 lines to the dataframe: the max and min of each variable to show on the plot!
 #summary_metrics <- rbind(rep(800,10) , rep(0,10) , summary_metrics)
 
-x <- data.frame(days_at_liberty=365, 
-                              mean_daily_distance = 80,
-                              mean_dist_to_shelf = 500,
-                              mean_distance_to_shore = 500,
-                              min_temp = 30,
-                              mean_temp = 30,
-                              max_temp = 30,
-                              min_depth = 100,
-                              mean_depth = 100,
-                              max_depth = 700, stringsAsFactors=FALSE)
+final_dat <- out_dat %>% select(-Ptt)
+  
+x <- data.frame(
+                DaysAtLiberty = max(out_dat$DaysAtLiberty)+10, 
+                mean_DailyDist = max(out_dat$mean_DailyDist)+10,
+                total_Dist = max(out_dat$total_Dist)+100,
+                mean_DistToShelf = max(out_dat$mean_DistToShelf)+10,
+                mean_DistToShore = max(out_dat$mean_DistToShore)+10,
+                min_Temp = max(out_dat$min_Temp)+4,
+                mean_Temp = max(out_dat$mean_Temp)+4,
+                max_Temp = max(out_dat$max_Temp)+4,
+                min_Depth = max(out_dat$min_Depth)+10,
+                mean_Depth = max(out_dat$mean_Depth)+10,
+                max_Depth = max(out_dat$max_Depth)+10, 
+                max_Bathy = max(out_dat$max_Bathy)+20, stringsAsFactors=FALSE)
 
-y <- data.frame(days_at_liberty=0, 
-                mean_daily_distance = 0,
-                mean_dist_to_shelf = 0,
-                mean_distance_to_shore = 0,
-                min_temp = 0,
-                mean_temp = 0,
-                max_temp = 0,
-                min_depth = 0,
-                mean_depth = 0,
-                max_depth = 0, stringsAsFactors=FALSE)
+y <- data.frame(
+                DaysAtLiberty=0, 
+                mean_DailyDist = 0,
+                total_Dist = 0,
+                mean_DistToShelf = 0,
+                mean_DistToShore = 0,
+                min_Temp = 0,
+                mean_Temp = 0,
+                max_Temp = 0,
+                min_Depth = 0,
+                mean_Depth = 0,
+                max_Depth = 0, 
+                max_Bathy = 0, stringsAsFactors=FALSE)
 
-summary_metrics <- rbind(x, y, summary_metrics)
-summary_metrics
+summary_metrics <- rbind(x, y, final_dat)
+summary_metrics 
 
-# Custom the radarChart !
-radarchart( summary_metrics  , axistype=1 , 
-            
-            #custom polygon
-            pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
-            
-            #custom the grid
-            cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.6,
-            
-            #custom labels
-            vlcex=0.9 
-)
+#-----------------------------------
+# THE PLOT FUNCTION SHOULD BE A SEPARATE SCRIPT CALLED AFTER THE ABOVE SCRIPT HAS RUN
+# Delete the loop when ready to split these scripts
 
-# #remotes::install_github("ricardo-bion/ggradar")
-# library(ggradar)
-# summary_metrics %>% ggradar(
-#   font.radar = "roboto",
-#   grid.label.size = 13,  # Affects the grid annotations (0%, 50%, etc.)
-#   axis.label.size = 8.5, # Afftects the names of the variables
-#   group.point.size = 3   # Simply the size of the point 
-# )
+for (k in 1:nrow(summary_metrics)){
+  
+  temp_metrics <- summary_metrics[k,]
+  
+  radarchart( temp_metrics  , axistype=1 , 
+              
+              #custom polygon
+              pcol=rgb(0.2,0.5,0.5,0.9) , pfcol=rgb(0.2,0.5,0.5,0.5) , plwd=4 , 
+              
+              #custom the grid
+              cglcol="grey", cglty=1, axislabcol="grey", cglwd=0.6,
+              
+              #custom labels
+              vlcex=0.9 
+  )
+  
+  
+  
+}
