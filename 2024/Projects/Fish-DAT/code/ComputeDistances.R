66171722d72aa5dfafc62d6eb6e77ed0e1073c63
lidefi87 <lidefi87@hotmail.com>
2024-05-09 15:20:59 +1000

    saving as example of function application

--- 2024/Projects/Fish-DAT/code/ComputeDistances.R
+++ 2024/Projects/Fish-DAT/code/ComputeDistances.R
@@ -1,75 +1,18 @@
-# Calculate:
-# Distance from shelf edge
-# Distance from shore
-# Distance from nearest estuary
-# by Fabrice Jaine
-# Last updated: May 2024
+base_folder <- "2024/Projects/Fish-DAT"
 
-# Initialise
-rm(list=ls(all.names = TRUE)) # to clear workspace
-setwd("~/Documents/GitHub/IMOS-hackathon/2024/Projects/Fish-DAT/data/") # set up your respective directory
+#Load scripts
+source(file.path(base_folder, "code/fishdat_functions.R"))
 
-#---- import data ----
-dat<-read.csv("227151/227151_daily-positions.csv",header=TRUE); head(dat)
+#Getting list of daily files
+daily_files <- list.files(base_folder, 
+                          pattern = "daily-positions.csv",
+                          recursive = T, full.names = T)
 
-# lookup local timezone
-library(lutz)
-timeZ <- tz_lookup_coords(lat = dat$Latitude, lon = dat$Longitude, method = "accurate"); 
-timeZ <- timeZ[1]
+#Output folder for bathymetry data
+save_bathy_path <- file.path(base_folder, "data")
 
-dat$Date <- as.POSIXct(as.character(dat$Date), format="%Y-%m-%d", tz=timeZ, origin = "1970-01-01")
-head(dat)
-
-#---- define ROI ----
-# Total ROI for bathymetry
-LatMin = ceiling(max(dat$Latitude)) + 2
-LatMax = floor(min(dat$Latitude)) - 2
-LonMin = floor(min(dat$Longitude)) - 2
-LonMax = ceiling(max(dat$Longitude)) + 2
-ROI = c(LonMin, LonMax, LatMin, LatMax)
-
-#---- load bathymetry data ----
-library(marmap)
-bat <- getNOAA.bathy(LonMin, LonMax, LatMin, LatMax, res = 1, keep=TRUE) 
-summary(bat)
-
-#--- Extract bathymetry and cumulative distance travelled at each position along the track ----
-dat.bathy <- get.depth(bat, x=dat$Longitude, y=dat$Latitude, distance=T, locator=F, res = 1) 
-colnames(dat.bathy) <- c("Longitude", "Latitude", "DistfromStart", "bathy_depth")
-head(dat.bathy)
-
-library(dplyr)
-dat <- left_join(dat,dat.bathy, by=c("Latitude","Longitude")); head(dat)
-  
-# library(remora)
-# dat <- extractEnv(df = dat,
-#                      X = "Longitude", 
-#                      Y = "Latitude", 
-#                      datetime = "Date", 
-#                      env_var = "bathy",  ## Currently only a single variable can be called at a time
-#                      cache_layers = TRUE,
-#                      crop_layers = FALSE,  
-#                      full_timeperiod = FALSE,
-#                      folder_name = "bathy_GA",
-#                      .parallel = TRUE)
-# head(dat)
-
-#---- Compute distances: ----
-x <- as.numeric(dat$Longitude) 
-y <- as.numeric(dat$Latitude)
-
-### Distance to nearest shelf edge (200m isobath):
-d_shelf <- dist2isobath(bat, x, y, isobath = -200) # distances between each sighting and nearest point on shelf edge
-d_shelf$distance <- d_shelf$distance/1000 # in km instead of metres
-DistToShelf <- d_shelf$distance
-
-### Distance to nearest coastline (0m isobath):
-d_shore <- dist2isobath(bat, x, y, isobath = 0) # distances between each sighting and nearest point on coastline
-d_shore$distance <- d_shore$distance/1000 # in km instead of metres
-DistToShore <- d_shore$distance
-
-dat <- cbind(dat, DistToShelf, DistToShore); head(dat) # merge outputs with main data frame
-
-#---- Save data ----
-write.csv(dat,paste0(unique(dat$Ptt),"/",unique(dat$Ptt),"","_daily-positions.csv"), row.names=F)
+#Apply computing function
+for(file in daily_files){
+  compute_distances(file, save_bathy_path)
+}
 
