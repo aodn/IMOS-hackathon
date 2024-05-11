2e3f74b88695d171712550af6ed03c3fe9c514a7
dahliasan <laninaesloco@gmail.com>
2024-05-11 23:32:23 +1000

    refactor to use bslib instead of shinydashboard

--- 2024/Projects/Fish-DAT/dashboard-app/R/dashboard_functions.R
+++ 2024/Projects/Fish-DAT/dashboard-app/R/dashboard_functions.R
@@ -45,3 +45,23 @@ create_colour_scale <- function(monthly_colour_palette) {
   colScale <- scale_colour_manual(name = "Month:", values = myColors)
   return(colScale)
 }
+
+
+create_sparkline <- function(data, x, y) {
+  data %>% 
+    plot_ly(x = ~get(x), y = ~get(y)) %>%
+    add_lines(
+      color = I("white"), span = I(1),
+      fill = 'tozeroy', alpha = 0.2
+    ) %>%
+    layout(
+      xaxis = list(visible = F, showgrid = F, title = ""),
+      yaxis = list(visible = F, showgrid = F, title = ""),
+      hovermode = "x",
+      margin = list(t = 0, r = 0, l = 0, b = 0),
+      font = list(color = "white"),
+      paper_bgcolor = "transparent",
+      plot_bgcolor = "transparent"
+    ) %>% 
+    config(displayModeBar = F)
+}
--- 2024/Projects/Fish-DAT/dashboard-app/app.R
+++ 2024/Projects/Fish-DAT/dashboard-app/app.R
@@ -1,4 +1,4 @@
-## Fish tracking dashboard 
+## Fish tracking dashboard
 ## Authors: Dahlia Foo, Dave Green, ChatGPT
 
 library(shiny)
@@ -6,90 +6,121 @@ library(shinyWidgets)
 library(leaflet)
 library(dplyr)
 library(ggplot2)
-library(shinydashboard)
+# library(shinydashboard)
 library(terra)
 # library(plotly)
 # library(shinyjs)
 # library(logging)
 library(ggiraph)
+library(bslib)
+library(bsicons)
+library(plotly)
 
-source(list.files(pattern="dashboard_setup.R", recursive=TRUE))
+source(list.files(pattern = "dashboard_setup.R", recursive = TRUE))
 
 # Set the relative path to the data folder
 base_folder <- ".."
 tag_ids <- metadata$Ptt
 species_id <- metadata$Species %>% unique()
 
-# Define UI
-header <- dashboardHeader(
-  title = "Fish Tracking Dashboard"
-)
 
-sidebar <- dashboardSidebar(
-  disable = TRUE
-)
+# ui ----------------------------------------------------------------------
 
-body <- dashboardBody(
-  tags$head(
-    tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+sidebar <- sidebar(
+  title = "Select individual",
+  pickerInput("species", "Species:",
+    choices = c("All", species_id),
+    options = list(`actions-box` = TRUE),
+    selected = "All"
   ),
-  fluidRow(
-    column(
-      width = 2,
-      pickerInput("species", "Species:",
-        choices = c("all", species_id),
-        options = list(`actions-box` = TRUE),
-        selected = "all"
-      ),
-      pickerInput("tagID", "Tag ID:",
-        choices = NULL,
-        options = list(`actions-box` = TRUE)
-      ),
-      infoBoxOutput("totalLength", width = NULL),
-      infoBoxOutput("deploymentLocation", width = NULL),
-      infoBoxOutput("deploymentDate", width = NULL),
-      infoBoxOutput("detachmentDate", width = NULL),
-      infoBoxOutput("detachmentLocation", width = NULL),
-    ),
-    column(
-      width = 10,
-      leafletOutput("map", height = "350px"),
-      # Insert attribution
-      tags$span(
-        "(Fishing data sourced from ",
-        tags$a(
-          href = "https://www.globalfishingwatch.org",
-          "Global Fishing Watch"
-          ), ")",
-        style = "font-size: 10px;"
-        ),
-      fluidRow(
-        column(
-          width = 6,
-          plotOutput("envPlot", height = "350px")
-        ),
-        column(
-          width = 6,
-          plotOutput("divePlot", height = "350px")
-        )
-      )
+  pickerInput("tagID", "Tag ID:",
+    choices = NULL,
+    options = list(`actions-box` = TRUE)
+  ),
+  card(
+    card_header("Total length"),
+    textOutput("totalLength"),
+  ),
+ card(
+   card_header("Deployment period"),
+   textOutput("deploymentPeriod")
+ ),
+ card(
+   card_header("Start location"),
+   textOutput("deploymentLocation")
+ ),
+ card(
+   card_header("End location"),
+   textOutput("detachmentLocation")
+ )
+)
+
+body <- layout_columns(
+  col_widths = c(12, 12, 6, 6),
+  fill = FALSE,
+ layout_columns(
+   fill = F,
+   value_box(
+     title = "Distance to Shelf",
+     value = textOutput("dist_to_shelf"),
+     showcase = plotlyOutput("dist_to_shelf_showcase"),
+     theme = "green",
+     showcase_layout = "bottom",
+     tags$p("(mean)", style = "font-size: 10px;")
+   ),
+   value_box(
+     title = "Mean Depth",
+     value = textOutput("mean_depth"),
+     showcase = plotlyOutput("mean_depth_showcase"),
+     theme = "green",
+     showcase_layout = "bottom",
+     tags$p("(mean)", style = "font-size: 10px;")
+   ),
+   value_box(
+     title = "Daily Distance Travelled",
+     value = textOutput("daily_dist"),
+     showcase = plotlyOutput("daily_dist_showcase"),
+     theme = "green",
+     showcase_layout = "bottom",
+     tags$p("(mean)", style = "font-size: 10px;")
+   ),
+ ),
+  card(
+    full_screen = T,
+    leafletOutput("map", height = "350px"),
+    # Insert attribution
+    tags$span(
+      "(Fishing data sourced from ",
+      tags$a(
+        href = "https://www.globalfishingwatch.org",
+        "Global Fishing Watch"
+      ), ")",
+      style = "font-size: 10px;"
     )
+  ),
+  card(
+    checkboxInput("show_active_date", "Show date marker on map hover", value = TRUE),
+    plotOutput("envPlot1", height = "350px")
+  ),
+  card(
+    plotOutput("divePlot", height = "350px")
   )
 )
 
-ui <- dashboardPage(
-  header, sidebar, body
-)
 
-# Define server logic
+ui <- page_sidebar(title = div("Fish tracking dashboard", tags$span(textOutput("active_individual"), style = "font-size: 10px;")), sidebar = sidebar, body)
+
+
+
+# server -----------------------------------------------------------------
+
+
 server <- function(input, output, session) {
   # Update species choices directly in the server
   # Observe any changes in the species selection
   observe({
     # Filter tag IDs based on selected species
-
-
-    if (input$species == "all") {
+    if (input$species == "All") {
       validTags <- metadata %>%
         pull(Ptt) %>%
         unique()
@@ -103,82 +134,93 @@ server <- function(input, output, session) {
     updatePickerInput(session, "tagID", choices = validTags)
   })
 
-  # infoBoxes
-  output$totalLength <- renderInfoBox({
+
+  # Sidebar info Cards --------------------------------------------------------------
+  individual_data <- reactive({
     req(input$tagID)
-    data <- metadata[metadata$Ptt == input$tagID, ]
-    infoBox(
-      title = HTML("Total<br>Length"),
-      value = paste0(data$TotalLength, "cm"),
-      icon = icon("ruler"),
-      color = "blue"
-    )
+    metadata %>% filter(Ptt == input$tagID)
   })
 
-  output$deploymentDate <- renderInfoBox({
-    req(input$tagID)
-    data <- metadata[metadata$Ptt == input$tagID, ]
-    infoBox(
-      title = HTML("Tagging<br> Date"),
-      value = format(data$Deployment_Date, "%d\n%b\n%Y"),
-      icon = icon("calendar-plus"),
-      color = "green"
-    )
+  output$active_individual <- renderText({
+    paste0("Now showing: ", individual_data()$Ptt, " (", individual_data()$Species, ")")
   })
 
-  output$detachmentDate <- renderInfoBox({
-    req(input$tagID)
-    data <- metadata[metadata$Ptt == input$tagID, ]
-    infoBox(
-      title = HTML("Pop-off<br>Date"),
-      value = format(data$Detachment_Date, "%d\n%b\n%Y"),
-      icon = icon("calendar-minus"),
-      color = "red"
-    )
+  output$totalLength <- renderText({
+    paste0(individual_data()$TotalLength, "cm")
   })
 
-  output$deploymentLocation <- renderInfoBox({
-    req(input$tagID)
-    data <- metadata[metadata$Ptt == input$tagID, ]
-    text <- paste(
-      # data$Deployment_Location, 
-      round(data$Deployment_Lat, 2), round(data$Deployment_Lon, 2), sep = ", ")
-    infoBox(
-      title = HTML("Tagging<br>Location"),
-      value = text,
-      icon = icon("map-marker"),
-      color = "green"
-    )
+  output$deploymentPeriod <- renderText({
+    paste0(format(individual_data()$Deployment_Date, "%d\n%b\n%Y"), " - ",
+           format(individual_data()$Detachment_Date, "%d\n%b\n%Y"))
   })
 
-  output$detachmentLocation <- renderInfoBox({
-    req(input$tagID)
-    data <- metadata[metadata$Ptt == input$tagID, ]
-    text <- paste(round(data$Detachment_Lat, 2), round(data$Detachment_Lon, 2), sep = ", ")
-    infoBox(
-      title = HTML("Pop-off<br>Location"),
-      value = text,
-      icon = icon("map-marker"),
-      color = "red"
-    )
+  output$deploymentLocation <- renderText({
+    paste(round(individual_data()$Deployment_Lat, 2), round(individual_data()$Deployment_Lon, 2), sep = ", ")
   })
 
+  output$detachmentLocation <- renderText({
+    paste(round(individual_data()$Detachment_Lat, 2), round(individual_data()$Detachment_Lon, 2), sep = ", ")
+  })
 
+  
   trackData <- reactive({
     req(input$tagID)
     load_track_data(input$tagID, base_folder, monthly_colour_palette)
   })
+  
 
+
+# Value boxes --------------------------------------------------
+
+  output$dist_to_shelf <- renderText({
+    track_sf <- trackData()$track_sf
+    paste0(mean(track_sf$DistToShelf, na.rm = T) %>%  round(0), " km")
+  })  
+  
+  output$dist_to_shelf_showcase <- renderPlotly({
+    track_sf <- trackData()$track_sf
+    create_sparkline(track_sf, x = "Date", y = "DistToShelf")
+
+  })
+  
+  output$mean_depth <- renderText({
+    track_sf <- trackData()$track_sf
+    paste0(mean(track_sf$MeanDepthPDT, na.rm = T) %>% round(0), " m")
+  })
+  
+  output$mean_depth_showcase <- renderPlotly({
+    track_sf <- trackData()$track_sf
+    create_sparkline(track_sf, x = "Date", y = "MeanDepthPDT")
+  })
+  
+  output$daily_dist <- renderText({
+    track_sf <- trackData()$track_sf
+    paste0(mean(track_sf$DistTravelled, na.rm = T) %>% round(0), " km")
+  })
+  
+  output$daily_dist_showcase <- renderPlotly({
+    track_sf <- trackData()$track_sf
+    create_sparkline(track_sf, x = "Date", y = "DistTravelled")
+  })
+  
+  
+  # Map ---------------------------------------------------------------------
+
+ 
+  
+  
 
   # Reactive expression to manage map data processing
   mapData <- reactive({
     req(trackData())
     trackData <- trackData()
+    
+    print(trackData()$track)
 
     if (is.null(trackData) == FALSE) {
       track_sf <- trackData$track_sf
       error_pols <- trackData$error_polys
-      fish_ras <- rast(trackData$fish_dat[c(2,1,3)], type = "xyz", crs = 'epsg:4326')
+      fish_ras <- rast(trackData$fish_dat[c(2, 1, 3)], type = "xyz", crs = "epsg:4326")
 
       mytext <- paste(
         "Date: ", track_sf$`Date`, "<br/>",
@@ -189,25 +231,25 @@ server <- function(input, output, session) {
         sep = ""
       ) %>%
         lapply(htmltools::HTML)
-      
-      fish_pal <- colorNumeric("Reds", values(fish_ras),  na.color = "transparent")
+
+      fish_pal <- colorNumeric("Reds", values(fish_ras), na.color = "transparent")
 
 
       leaflet() %>%
         addProviderTiles(providers$Esri.WorldImagery, group = "Satellite") %>%
         addProviderTiles(providers$OpenStreetMap, group = "Map") %>%
         addRasterImage(
-          x = fish_ras, 
+          x = fish_ras,
           opacity = 0.5,
           color = fish_pal,
           layerId = "Fishing effort", group = "Fishing effort"
-        ) %>% 
+        ) %>%
         addPolygons(
           data = error_pols,
           stroke = T, weight = 1, color = "grey",
           fillOpacity = 0.15, fillColor = "lightgrey",
           labelOptions = labelOptions(noHide = TRUE)
-        )  %>% 
+        ) %>%
         addPolylines(
           lng = track_sf$Longitude, lat = track_sf$Latitude,
           color = "white", weight = 1.5,
@@ -221,11 +263,12 @@ server <- function(input, output, session) {
           label = mytext,
           layerId = track_sf$`Date`
         ) %>%
-        addLegend(pal = fish_pal, values = values(fish_ras), 
-                  title = "Fishing<br>effort (h)", layerId = "Fishing effort",
-                  position = "bottomright", opacity = 1, group = "Fishing effort",
-                  className = "custom-leaflet-legend"
-        ) %>% 
+        addLegend(
+          pal = fish_pal, values = values(fish_ras),
+          title = "Fishing<br>effort (h)", layerId = "Fishing effort",
+          position = "bottomright", opacity = 1, group = "Fishing effort",
+          className = "custom-leaflet-legend"
+        ) %>%
         addLegend(
           position = "bottomleft",
           colors = unique(track_sf$colour), labels = unique(track_sf$month),
@@ -237,9 +280,8 @@ server <- function(input, output, session) {
           # baseGroups = as.character(unique(track_sf$month)),
           baseGroups = c("Satellite", "Map"),
           options = layersControlOptions(collapsed = FALSE)
-        ) %>% 
+        ) %>%
         hideGroup("Fishing effort")
-      
     } else {
       leaflet() %>% addProviderTiles(providers$Esri.WorldImagery)
     }
@@ -251,6 +293,11 @@ server <- function(input, output, session) {
     mapData() # This will only recompute when input$tagID changes or when the data changes
   })
 
+
+
+  # Plots -------------------------------------------------------------------
+
+
   # Generate highlightData in reactive instead
   highlightData <- reactive({
     req(trackData())
@@ -261,12 +308,10 @@ server <- function(input, output, session) {
   })
 
   # Plot for individual fish
-  output$envPlot <- renderPlot({
-
+  output$envPlot1 <- renderPlot({
     track <- trackData()$track
-    print(track)
 
-    is_highlighted <- nrow(highlightData()) > 0
+    if (input$show_active_date) is_highlighted <- nrow(highlightData()) > 0
 
     ggplot(track, aes(x = Date)) +
       geom_ribbon(aes(ymin = MinTemp, ymax = MaxTemp), fill = "grey", col = NA, alpha = 0.5) +
@@ -274,54 +319,85 @@ server <- function(input, output, session) {
       geom_path(aes(y = MinTemp), linetype = 3, col = "grey60", na.rm = TRUE) +
       geom_path(aes(y = MaxTemp), linetype = 3, col = "grey60", na.rm = TRUE) +
       theme_minimal() +
-      list(if(is_highlighted) geom_point(data = highlightData(), aes(x = Date, y = MeanTempPDT), color = "red"))
+      scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+      list(if (input$show_active_date && is_highlighted) geom_vline(data = highlightData(), aes(xintercept = Date), color = "red"))
+  })
+
+
+  # Plot for individual fish
+  output$envPlot2 <- renderPlot({
+    track <- trackData()$track
+    print(track)
+
+    if (input$show_active_date) is_highlighted <- nrow(highlightData()) > 0
+
+    ggplot(track, aes(x = Date)) +
+      geom_ribbon(aes(ymin = MinTemp, ymax = MaxTemp), fill = "grey", col = NA, alpha = 0.5) +
+      geom_path(aes(y = MeanDepthPDT), color = track$colour, na.rm = TRUE) +
+      geom_path(aes(y = MinDepth), linetype = 3, col = "grey60", na.rm = TRUE) +
+      geom_path(aes(y = MaxDepth), linetype = 3, col = "grey60", na.rm = TRUE) +
+      theme_minimal() +
+      list(if (input$show_active_date && is_highlighted) geom_vline(data = highlightData(), aes(xintercept = Date), color = "red"))
   })
 
   # Bar plot of dive depth
   output$divePlot <- renderPlot({
-    if(is_empty(input$tagID)) return()
+    if (is_empty(input$tagID)) {
+      return()
+    }
     histoplot(tag_ids = as.character(input$tagID), folder_path = "../data")
   })
- 
+
+
+  # output$seriesPlot <- renderPlot({
+  #
+  #   p <- ggplot(subsetted(), aes(!!input$xvar, !!input$yvar)) + list(
+  #     theme(legend.position = "bottom"),
+  #     if (input$by_species) aes(color = Species),
+  #     geom_point(),
+  #     if (input$smooth) geom_smooth()
+  #   )
+  #
+  #
+  # })
+
   ## Plot for individual fish
   # output$envPlot <- renderPlot({
   #   if(is_empty(input$tagID)) return()
-  #   
+  #
   #   h <- histoviz("../data")
-  #   
+  #
   #   ## summarise variables to larger time steps (daily) so it plots nicer
   #   h_summary <-
   #     h %>%
-  #     filter(id %in% input$tagID) %>% 
-  #     mutate(date = date(date)) %>% 
-  #     group_by(id, date, bin, variable) %>% 
+  #     filter(id %in% input$tagID) %>%
+  #     mutate(date = date(date)) %>%
+  #     group_by(id, date, bin, variable) %>%
   #     summarise(prop = mean(prop))
-  #   
+  #
   #   a <-
-  #     h_summary %>% 
-  #     filter(variable %in% "depth") %>% 
+  #     h_summary %>%
+  #     filter(variable %in% "depth") %>%
   #     ggplot(aes(x = date, y = fct_rev(bin), fill = prop, id = variable)) +
   #     geom_tile() +
   #     scale_x_date(expand = c(0,0)) +
   #     scale_fill_viridis_c(direction = -1, na.value = NA, limits = c(0,100)) +
   #     labs(x = "Date", y = "Depth (m)", fill = "Proportion\nof time") +
   #     theme_bw()
-  #   
-  #   b <- 
-  #     h_summary %>% 
-  #     filter(variable %in% "temp") %>% 
+  #
+  #   b <-
+  #     h_summary %>%
+  #     filter(variable %in% "temp") %>%
   #     ggplot(aes(x = date, y = bin, fill = prop, id = variable)) +
   #     geom_tile() +
   #     scale_x_date(expand = c(0,0)) +
   #     scale_fill_viridis_c(option = "A", direction = -1, na.value = NA, limits = c(0,100)) +
   #     labs(x = "Date", y = "Temperature (˚C)", fill = "Proportion\nof time") +
   #     theme_bw()
-  #   
+  #
   #   a/b
   # })
-  
 }
 
 # Run the application
 shinyApp(ui = ui, server = server)
-
